e8c1fc1186290c5df488314307eacdc2d034fedc
drussier <damien.russier@ogury.co>
2018-10-18 16:18:37 +0000

    running 500 000 apps

---
+++ lda/src/prepare_input_sagemaker.py
@@ -0,0 +1,9 @@
+# coding: utf-8
+import pickle
+
+
+def convert_mallet_corpus_to_sm_corpus():
+    pass
+
+if __name__ == '__main__':
+    pass
--- lda/src/run_gensim_lda.py
+++ lda/src/run_gensim_lda.py
@@ -1,12 +1,15 @@
 # coding: utf-8
 import pandas as pd
 import numpy as np
+from tabulate import tabulate
 
+import os
 from pathlib import Path
 from time import time
 from subprocess import Popen, PIPE, STDOUT
 import simplejson
 import pickle
+import re
 
 import nltk
 nltk.download('wordnet')
@@ -47,7 +50,8 @@ def jz_read(fn):
             # https://serverfault.com/questions/270814/fastest-way-to-extract-tar-gz
             # p1 = Popen(['pigz', '-dc'], stdin=f, stdout=PIPE)
             # p2 = Popen([])
-            p = Popen(['tar', '-I', 'pigz', '-xf'], stdin=f, stdout=PIPE)
+            # p = Popen(['tar', '-I', 'pigz', '-xf'], stdin=f, stdout=PIPE)
+            p = Popen(['tar', '-I', 'pigz', '-xf', fn], stdout=PIPE)            
             return simplejson.loads(p.communicate()[0])
         else :
             p = Popen(['pigz', '-dc'], stdin=f, stdout=PIPE)
@@ -62,6 +66,15 @@ def print_top_words(model, feature_names, n_top_words):
         print(message)
 
     print()
+
+
+def remove_url(text):
+    """ remove string with http, .com or www
+    """
+    text = re.sub(r"\S*http\S+", "", text)
+    text = re.sub(r"\S*\.com\S*", "", text)
+    text = re.sub(r"\S*www\S+", "", text)
+    return text
 
 
 def tokenize(text):
@@ -79,6 +92,7 @@ def tokenize(text):
     return lda_tokens
 
 
+
 def get_lemma(word):
     lemma = wn.morphy(word)
     if lemma is None:
@@ -92,6 +106,7 @@ def get_lemma2(word):
 
 
 def prepare_text_for_lda(text, lemmatize=get_lemma):
+    text = remove_url(text)
     tokens = tokenize(text)
     tokens = [token for token in tokens if len(token) > 4]
     tokens = [token for token in tokens if token not in en_stop]
@@ -102,15 +117,24 @@ def prepare_text_for_lda(text, lemmatize=get_lemma):
 
 if __name__ == '__main__':
     # Read data    
-    # raw_data = jz_read(DATA + 'matters.en.2018-06.json.gz')
-    raw_data = jz_read(DATA + 'playstore-00.tar.gz')
+    raw_data = jz_read(DATA + 'matters.en.2018-09.json.gz')
+    # raw_data = jz_read(DATA + 'playstore-00.tar.gz')
 
-    df = pd.DataFrame.from_dict(raw_data, orient='index')
-    print(0, df.head())
+    cols = ['title', 'category', 'cat_key', 'description', 'short_desc']
+    try : 
+        df = pd.DataFrame.from_dict(raw_data, orient='index')[cols + ['lang']]
+        print('{} apps loaded'.format(len(df)))
+        print('{} non-english apps removed'.format(len(df[df['lang'] != 'en'])))
+        df = df[df['lang'] == 'en']
+    except KeyError:
+        df = pd.DataFrame.from_dict(raw_data, orient='index')[cols]
+    stop
+    
+    
+    print(tabulate(df.head(), headers='keys', tablefmt='psql'))
     
     # random shuffle
     df = df.sample(frac=1, random_state=0)
-    print(1, df.head())
 
     # remove uncategorized
     # df = df[df['cat_key'] != 'UNCATEGORIZED']
@@ -124,7 +148,7 @@ if __name__ == '__main__':
         df = df[~ df['cat_key'].str.startswith('GAME_')]
         print(len(df))
 
-    dff = df.copy().iloc[:1000]
+    dff = df.copy() # .iloc[:1000]
     print('{} documents'.format(len(dff)))
     
     dff['tokens'] = dff.apply(
@@ -133,35 +157,43 @@ if __name__ == '__main__':
 
     dff['nb_of_tokens'] = dff['tokens'].apply(lambda lst : len(lst))
     dff = dff[dff['nb_of_tokens'] > 0]
+    dff.sort_values('nb_of_tokens', inplace=True, ascending=False)
+
+    # keep 500000 longest texts
+    dff = dff.iloc[:500000]
     
     BASE_NAME = DATA + 'gensim/raw_desc_{}samples'.format(len(dff))
     # df.to_csv(BASE_NAME + ".df.bz2", compression='bz2')
     dff.to_csv(BASE_NAME + ".dff.bz2", compression='bz2')
 
     # save list of nb of tokens to plot distribution
-    with open(BASE_NAME + "nb_of_tokens.list", "wb") as fp:
+    with open(BASE_NAME + "_nb_of_tokens.list", "wb") as fp:
         pickle.dump(dff['nb_of_tokens'], fp)
 
-    stop
-    
     text_data = dff['tokens'].tolist()
     # print(text_data[:5])
+
+    print('creating dictionary...')
     dictionary = corpora.Dictionary(text_data)
+    print('creating corpus...')    
     corpus = [dictionary.doc2bow(text) for text in text_data]
     
-
     pickle.dump(corpus, open(BASE_NAME + '.corpus', 'wb'))
     dictionary.save(BASE_NAME + '.dict')
     
     # optimal number of topics is 50 without games
-    for NUM_TOPICS in [len(df['cat_key'].unique()), 100]:
+    for NUM_TOPICS in [100] : # [len(df['cat_key'].unique()), 100]:
         print('*** Mallet === {} topics'.format(NUM_TOPICS))
         # https://www.machinelearningplus.com/nlp/topic-modeling-gensim-python/
         model = gensim.models.wrappers.LdaMallet(MALLET_PATH, corpus=corpus, num_topics=NUM_TOPICS, id2word=dictionary)
+        
+        # save files needed to load the model
         model.save(BASE_NAME + '_{}topics.model_mallet_nogames'.format(NUM_TOPICS))
+        os.system('cp {} {}'.format(model.fcorpusmallet(), DATA + 'gensim/'))
+        os.system('cp {} {}'.format(model.fcorpustxt(), DATA + 'gensim/'))
 
     # gensim implementation
-    for NUM_TOPICS in [len(df['cat_key'].unique()), 100]:
+    for NUM_TOPICS in [] : # [len(df['cat_key'].unique()), 100]:
         print('*** Gensim === {} topics'.format(NUM_TOPICS))
         # default parameters
         model = gensim.models.ldamodel.LdaModel(corpus, num_topics=NUM_TOPICS, id2word=dictionary, passes=1, random_state=123456, chunksize=10000)
